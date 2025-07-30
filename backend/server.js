7c8b33700b4fc026737af7c5b77b4d73407f81dd
Johny Noise <79932410+johnynoise@users.noreply.github.com>
2025-07-30 08:26:59 -0400

    ✨ CRUD DE USUARIOS

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,47 @@
 import express from 'express';
+import { PrismaClient } from '../backend/generated/prisma/index.js';
+
+const prisma = new PrismaClient();
 
 const app = express();
+app.use(express.json());
 
-app.get('/usuarios', (req, res) => {
-  res.send('Hello, World!');
-}); 
 
-app.post('/usuarios', (req, res) => {
-  res.send('User created successfully!');
+app.get('/usuarios', async (req, res) => {
+  const users = await prisma.user.findMany();
+  res.json(users);
+  
+});
+app.post('/usuarios', async (req, res) => {
+  await prisma.user.create({
+    data: {
+      email: req.body.email,
+      name: req.body.name,
+      empresa: req.body.empresa
+    }
+  })
+  res.status(201).send('User added successfully!');
 });
 
-app.put('/usuarios/:id', (req, res) => {
+app.put('/usuarios/:id', async (req, res) => {
   const userId = req.params.id;
-  res.send(`User with ID ${userId} updated successfully!`);
+  await prisma.user.update({
+    where: { id: userId },
+    data: {
+      email: req.body.email,
+      name: req.body.name,
+      empresa: req.body.empresa
+    }
+  });
+  res.status(200).send(`User with ID ${userId} updated successfully!`);
 });
 
-app.delete('/usuarios/:id', (req, res) => {
+app.delete('/usuarios/:id', async (req, res) => {
   const userId = req.params.id;
-  res.send(`User with ID ${userId} deleted successfully!`);
+  await prisma.user.delete({
+    where: { id: userId }
+  });
+  res.status(204).send(`User with ID ${userId} deleted successfully!`);
 });
 
 app.listen(3000, () => {
